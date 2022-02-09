ce8626b1cf5a0994caa2803f80e26531a73c340c
flavia <flaviarosadejesus@hotmail.com>
2022-02-09 19:26:29 -0300

    OK

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cadastro de clientes");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+        }
+   
+
+static void Main
+Console.WriteLine("Digite nome: ");
+string nome = console.ReadLine();
+Console.WriteLine("Digite cpf: ");
+string cpf = console.ReadLine();
+
+
+
+Console.Writeline(" Lista: ");
+Console.Writeline(" Lista: ");
+Console.Writeline(" Lista: ");
+Console.Writeline(" Lista: ");
+Console.Writeline(nome1+ +cpf1);
+
+ }
+}
